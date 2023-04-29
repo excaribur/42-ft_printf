2950d761f82970e8aba9f13fc4504de7154a1da9
Jirayut Phonyiam <jphonyia@z2t17c2.42bangkok.com>
2023-04-29 15:24:58 +0700

    update: remove .o

--- main.c
+++ main.c
@@ -104,14 +104,14 @@ int	main()
 
 // #include <stdio.h>
 // #include <stdint.h>
-       printf("r: %d\n", printf("r: %c %s %d %i %u %x %X %p %% \n", NULL, NULL, NULL, NULL, NULL, NULL, NULL));
+	printf("r: %d\n", printf("r: %c %s %d %i %u %x %X %p %% \n", NULL, NULL, NULL, NULL, NULL, NULL, NULL));
     printf("f: %d\n", ft_printf("f: %c %s %d %i %u %x %X %p %% \n", NULL, NULL, NULL, NULL, NULL, NULL, NULL));
-    //printf("r: %d\n", printf("r: %c %s %d %i %u %x %X %p %% \n", 'G',"hello", -123, -123, -123, -123, -123, "hello"));
-    //printf("f: %d\n", ft_printf("f: %c %s %d %i %u %x %X %p %% \n", 'G',"hello", -123, -123, -123, -123, -123, "hello"));
-    //printf("r: %d\n", printf("r: %c %s %d %i %u %x %X %p %% \n", 'G',"hello", INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, "hello"));
-    //printf("r: %d\n", ft_printf("r: %c %s %d %i %u %x %X %p %% \n", 'G',"hello", INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, "hello"));
-    //printf("r: %d\n", printf("r: %c %s %d %i %u %x %X %p %% \n", 'G',"hello", INT32_MIN, INT32_MIN, INT32_MIN,INT32_MIN, INT32_MIN, "hello"));
-    //printf("r: %d\n", ft_printf("r: %c %s %d %i %u %x %X %p %% \n", 'G',"hello", INT32_MIN, INT32_MIN, INT32_MIN,INT32_MIN, INT32_MIN, "hello"));
+    printf("r: %d\n", printf("r: %c %s %d %i %u %x %X %p %% \n", 'G',"hello", -123, -123, -123, -123, -123, "hello"));
+    printf("f: %d\n", ft_printf("f: %c %s %d %i %u %x %X %p %% \n", 'G',"hello", -123, -123, -123, -123, -123, "hello"));
+    printf("r: %d\n", printf("r: %c %s %d %i %u %x %X %p %% \n", 'G',"hello", INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, "hello"));
+    printf("r: %d\n", ft_printf("r: %c %s %d %i %u %x %X %p %% \n", 'G',"hello", INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, INT32_MAX, "hello"));
+    printf("r: %d\n", printf("r: %c %s %d %i %u %x %X %p %% \n", 'G',"hello", INT32_MIN, INT32_MIN, INT32_MIN,INT32_MIN, INT32_MIN, "hello"));
+    printf("r: %d\n", ft_printf("r: %c %s %d %i %u %x %X %p %% \n", 'G',"hello", INT32_MIN, INT32_MIN, INT32_MIN,INT32_MIN, INT32_MIN, "hello"));
 
 	return 0;
 }
